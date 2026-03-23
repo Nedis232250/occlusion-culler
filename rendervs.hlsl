c06b02c39cca96ab45ba32748b7cfb72748b1a62
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2026-03-23 17:19:34 -0400

    Add vertex shader implementation in rendervs.hlsl

---
+++ rendervs.hlsl
@@ -0,0 +1,40 @@
+StructuredBuffer<float> vertices : register(t0);
+ByteAddressBuffer triangles : register(t1);
+
+struct VSOutput {
+    float4 position : SV_POSITION;
+    float4 color : COLOR0;
+};
+
+#define rx 0.0h
+#define ry 0.0h
+
+VSOutput main(uint vertexID : SV_VertexID) {
+    VSOutput output;
+    if (triangles.Load(4 * (vertexID / 3)) == 0) {
+        output.position = float4(0.0f, 0.0f, 0.0f, 0.0f);
+        output.color = float4(0.0f, 0.0f, 0.0f, 1.0f);
+        return output;
+    }
+    
+    uint starting_index = vertexID * 7;
+    
+    half3x3 mx = half3x3(
+        half3(1.0h, 0.0h, 0.0h),
+        half3(0.0h, cos(rx), -sin(rx)),
+        half3(0.0h, sin(rx), cos(rx))
+    );
+    
+    half3x3 my = half3x3(
+        half3(cos(ry), 0.0h, sin(ry)),
+        half3(0.0h, 1.0h, 0.0h),
+        half3(-sin(ry), 0.0h, cos(ry))
+    );
+    
+    half3 coordinates = half3(vertices[starting_index], vertices[starting_index + 1], vertices[starting_index + 2] * 0.01h);
+    coordinates = mul(mul(my, mx), coordinates);
+    
+    output.position = float4(coordinates, 0.1h * (coordinates.z * 100.0h + 10.0h));
+    output.color = float4(vertices[starting_index + 3], vertices[starting_index + 4], vertices[starting_index + 5], vertices[starting_index + 6]);
+    return output;
+}
