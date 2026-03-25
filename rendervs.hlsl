c0c35eaa2cedb33a00eb6bbd9f6502456d6cc80a
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2026-03-24 20:33:50 -0400

    Add cull_buffer and update vertex shader logic

--- rendervs.hlsl
+++ rendervs.hlsl
@@ -1,17 +1,18 @@
 StructuredBuffer<float> vertices : register(t0);
 ByteAddressBuffer triangles : register(t1);
+StructuredBuffer<uint> cull_buffer : register(t2);
 
 struct VSOutput {
     float4 position : SV_POSITION;
     float4 color : COLOR0;
 };
 
-#define rx 0.0h
+#define rx radians(0.0h)
 #define ry 0.0h
 
 VSOutput main(uint vertexID : SV_VertexID) {
     VSOutput output;
-    if (triangles.Load(4 * (vertexID / 3)) == 0) {
+    if (triangles.Load(4 * (vertexID / 3)) == 0 && cull_buffer[vertexID / 3] == 1) {
         output.position = float4(0.0f, 0.0f, 0.0f, 0.0f);
         output.color = float4(0.0f, 0.0f, 0.0f, 1.0f);
         return output;
@@ -34,6 +35,7 @@ VSOutput main(uint vertexID : SV_VertexID) {
     half3 coordinates = half3(vertices[starting_index], vertices[starting_index + 1], vertices[starting_index + 2] * 0.01h);
     coordinates = mul(mul(my, mx), coordinates);
     
+    //coordinates.y -= 0.5h;
     output.position = float4(coordinates, 0.1h * (coordinates.z * 100.0h + 10.0h));
     output.color = float4(vertices[starting_index + 3], vertices[starting_index + 4], vertices[starting_index + 5], vertices[starting_index + 6]);
     return output;
